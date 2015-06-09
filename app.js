61addb09bc474de58a4b3461aca1b07e2b033460
Junfeng Li <autozimu@gmail.com>
2015-06-08 20:51:08 -0400

    Volume Meter.
    Ref: https://webaudiodemos.appspot.com/volume-meter/index.html

--- app.js
+++ app.js
@@ -1,31 +1,54 @@
-window.AudioContext = window.AudioContext || window.webkitAudioContext;
-navigator.getUserMedia = navigator.getUserMedia || navigator.webkitGetUserMedia || navigator.mozGetUserMedia || navigator.msGetUserMedia;
-navigator.cancelAnimationFrame = navigator.cancelAnimationFrame || navigator.webkitCancelAnimationFrame || navigator.mozCancelAnimationFrame;
-navigator.requestAnimationFrame = navigator.requestAnimationFrame || navigator.webkitRequestAnimationFrame || navigator.mozRequestAnimationFrame;
-
-
-var audioCtx = new AudioContext();
-
-var analyser = audioCtx.createAnalyser();
-
-var distortion = audioCtx.createWaveShaper();
-var gainNode = audioCtx.createGain();
-var biquadFiler = audioCtx.createBiquadFilter();
-var convolver = audioCtx.createConvolver();
-
-if (!navigator.getUserMedia) {
-	throw new Error("getUserMedia not supported");
+// Request media API.
+navigator.mediaDevices = navigator.mediaDevices || ((navigator.mozGetUserMedia || navigator.webkitGetUserMedia) ? {
+	getUserMedia: function(c) {
+		return new Promise(function(y, n) {
+			(navigator.mozGetUserMedia || navigator.webkitGetUserMedia).call(navigator, c, y, n);
+		});
+	}
+} : null);
+// Ensure getUserMedia() is supported.
+if (!navigator.mediaDevices) {
+	console.log("getUserMedia() not supported.");
+	throw new Error("getUserMedia() not supported.");
 }
 
-navigator.getUserMedia(
+// AudioContext API.
+window.AudioContext = window.AudioContext || window.webkitAudioContext;
+var audioCtx = new AudioContext();
+
+var mediaStreamSource;
+var meter;
+var raftID;
+var meterCanvas;
+
+navigator.mediaDevices.getUserMedia(
 	{
 		audio: true,
-		video: true
-	},
-	function (stream) {
-		document.querySelector('#camFeed').src = window.URL.createObjectURL(stream);
-	},
-	function (err) {
-		console.log("The following error occured: " + err.name);
 	}
-);
+)
+.then(function (stream) {
+	meterCanvas = document.querySelector("#meter").getContext("2d");
+	mediaStreamSource = audioCtx.createMediaStreamSource(stream);
+	meter = createAudioMeter(audioCtx);
+	mediaStreamSource.connect(meter);
+
+	drawLoop();
+})
+.catch(function (err) {
+	console.log("The following error occured: " + err.name);
+});
+
+function drawLoop(time) {
+	meterCanvas.clearRect(0, 0, meterCanvas.canvas.width, meterCanvas.canvas.height);
+	
+	if (meter.checkClipping()) {
+		meterCanvas.fillStyle = "#B20000";
+	}
+	else {
+		meterCanvas.fillStyle = "#00FF48";
+	}
+	
+	meterCanvas.fillRect(0, 0, meter.volume * meterCanvas.canvas.width * 1.4, meterCanvas.canvas.height);
+	
+	raftID = window.requestAnimationFrame(drawLoop);
+}
